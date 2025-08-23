1e38e0837c8f8c333f8d6a64547193cac2722900
Mansi Sanjay Patil <sanjaymansipatil@gmail.com>
2025-08-23 14:31:55 +0530

    Entities of Transaction,User,Order and TransactionStatus Enum

--- src/main/java/com/yash/cafeconnect/entity/Order.java
+++ src/main/java/com/yash/cafeconnect/entity/Order.java
@@ -1,4 +1,44 @@
 package com.yash.cafeconnect.entity;
 
 public class Order {
+
+    private int orderId;
+    private String userEmailId;
+    private String cafeId;
+    private float totalAmount;
+
+    public Order(int orderId, String userEmailId, String cafeId, float totalAmount)
+    {
+        this.orderId = orderId;
+        this.userEmailId = userEmailId;
+        this.cafeId = cafeId;
+        this.totalAmount = totalAmount;
+
+    }
+
+    public int getOrderId() {
+            return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+    public String getUserEmailId() {
+        return userEmailId;
+    }
+    public void setUserEmailId(String userEmailId) {
+        this.userEmailId = userEmailId;
+    }
+    public String getCafeId() {
+        return cafeId;
+    }
+    public void setCafeId(String cafeId) {
+        this.cafeId = cafeId;
+    }
+    public float getTotalAmount() {
+        return totalAmount;
+    }
+    public void setTotalAmount(float totalAmount) {
+        this.totalAmount = totalAmount;
+    }
 }
---
+++ src/main/java/com/yash/cafeconnect/entity/Transaction.java
@@ -0,0 +1,87 @@
+package com.yash.cafeconnect.entity;
+import com.yash.cafeconnect.entity.enums.TransactionStatus;
+
+import java.time.Instant;
+
+import java.util.Date;
+
+public class Transaction {
+    private int transactionId;
+    private float amount;
+    private String paymentMethod;
+    private TransactionStatus transactionStatus;
+    private Instant timeStamp;
+    private String userEmailId;
+    private int itemId;
+    private int orderId;
+
+    public Transaction(int transactionId,float amount,String paymentMethod, TransactionStatus transactionStatus,Instant timeStamp,String userEmailId, int itemId, int orderId){
+        this.transactionId = transactionId;
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+        this.transactionStatus = transactionStatus;
+        this.timeStamp = timeStamp;
+        this.userEmailId = userEmailId;
+        this.itemId = itemId;
+        this.orderId = orderId;
+
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public TransactionStatus getTransactionStatus() {
+        return transactionStatus;
+    }
+
+    public void setTransactionStatus(TransactionStatus transactionStatus) {
+        this.transactionStatus = transactionStatus;
+    }
+
+    public Instant getTimeStamp() {
+        return timeStamp;
+    }
+    public void setTimeStamp(Instant timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getUserEmailId() {
+        return userEmailId;
+    }
+    public void setUserEmailId(String userEmailId) {
+        this.userEmailId = userEmailId;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+}
