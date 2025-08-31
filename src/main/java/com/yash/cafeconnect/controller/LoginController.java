1e7abbb864db10c9a4248a1307bc56ed6f7037ef
Mehul  Jain <mehul.jain@Yash.com>
2025-08-31 12:36:20 +0530

    New login controller added

---
+++ src/main/java/com/yash/cafeconnect/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.yash.cafeconnect.controller;
+
+import jakarta.servlet.http.HttpServlet;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+}
