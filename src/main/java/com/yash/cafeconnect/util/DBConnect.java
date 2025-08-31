45928a58770b1f19100cb7e9b55d7c895a9a4b87
Mehul Jain <97732513+mehuljain55@users.noreply.github.com>
2025-08-31 13:59:35 +0530

    Update DBConnect.java

--- src/main/java/com/yash/cafeconnect/util/DBConnect.java
+++ src/main/java/com/yash/cafeconnect/util/DBConnect.java
@@ -6,10 +6,10 @@ public class DBConnect {
     private  static final String URL = "jdbc:mysql://localhost:3306/cafe_connect";
 
     /** Database username */
-    private static final String USER = "root";
+    private static final String USER = "cafe_manager";
 
     /** Database password */
-   private static final String PASSWORD = "root";
+   private static final String PASSWORD = "cafe@2025";
 
    private static  Connection con = null;
 
