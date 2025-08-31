00c47523fe20b6dd17598017149ce31612e91ac8
Swarupa Jaliminche <136915278+SwarupaJ@users.noreply.github.com>
2025-08-31 23:42:36 +0530

    written dao implementation and service implementation

--- src/main/java/com/yash/cafeconnect/service/CafeService.java
+++ src/main/java/com/yash/cafeconnect/service/CafeService.java
@@ -1,4 +1,22 @@
 package com.yash.cafeconnect.service;
 
+import com.yash.cafeconnect.entity.Cafe;
+import java.util.List;
+
 public interface CafeService {
+
+    //Add a new Cafe
+    void addCafe(Cafe cafe);
+
+    //Get cafe by Id
+    Cafe getCafeById(int cafeId);
+
+    //Update cafe details
+    void updateCafeById(Cafe cafe);
+
+    //Remove Cafe by Id
+    void removeCafeById(int cafeId);
+
+    //Get list of all cafes
+    List<Cafe> getCafes();
 }
--- src/main/java/com/yash/cafeconnect/serviceImpl/OfficeServiceImpl.java
+++ src/main/java/com/yash/cafeconnect/serviceImpl/OfficeServiceImpl.java
@@ -1,4 +1,47 @@
-package com.yash.cafeconnect.serviceImpl;
-
-public class OfficeServiceImpl {
-}
+//package com.yash.cafeconnect.serviceImpl;
+//
+//import com.yash.cafeconnect.entity.Office;
+//import com.yash.cafeconnect.dao.OfficeDao;
+//import com.yash.cafeconnect.daoImpl.OfficeDaoImpl;
+//import com.yash.cafeconnect.service.OfficeService;
+//
+//import java.util.List;
+//
+//public class OfficeServiceImpl implements OfficeService
+//{
+//    private OfficeDaoImpl officeDao = new OfficeDaoImpl();
+//
+//    public void addOffice(Office office){
+//        if (office.getOfficeName() == null || office.getOfficeName().isEmpty()){
+//            throw new IllegalArgumentException("office name cannot be empty");
+//        }
+//
+//        if(officeDao.getOffice(office.getOfficeId()) !=null){
+//            throw new IllegalStateException("office with this Id already exists");
+//        }
+//        officeDao.saveOffice(office);
+//    }
+//
+//    public Office getOfficeById(int officeId){
+//        return officeDao.getOffice(officeId);
+//    }
+//
+//    public void updateOfficeById(Office office){
+//        if (officeDao.getOffice(office.getOfficeId()) == null){
+//            throw new IllegalStateException("Office with this Id does not exist");
+//        }
+//        officeDao.UpdateOffice(office);
+//    }
+//
+//    public void removeOfficeById(int officeId) {
+//        if (officeDao.getOffice(officeId) == null) {
+//            throw new IllegalStateException("Office with this Id does not exist");
+//        }
+//        officeDao.deleteOffice(officeId);
+//    }
+//
+////    public List<Office> getOffices() {
+////        return officeDao.getOffices();
+////    }
+//
+//}
